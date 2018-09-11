af5ac64dddeefe49113b8652af121511175eb85d
univer <univer@AppledeMacBook-Pro.local>
2018-09-11 10:39:24 +0800

    更新获取数据

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 module.exports = {
-	fetchData( dataUrl, start ) {
-		return new Promise((resolve, reject) => {
-			fetch(`${dataUrl}/data${(start+ 0 + '').padStart(2, '0')}.json`)
-				.then((data) => {
-					resolve(data.json());
-				}, (error) => {
-					reject(error);
-				})
-		});
+    fetchData( dataUrl, start ) {
+        return new Promise((resolve, reject) => {
+            fetch(`${dataUrl}/data${(start+ 0 + '').padStart(2, '0')}.json`)
+                .then((data) => {
+                    resolve(data.json());
+                }, (error) => {
+                    reject(error);
+                })
+        });
 
-	},
+    },
 
     async getData( dataUrl, start ) {
 
-		const _self = this;
-		let data = await _self.fetchData( dataUrl, start );
-		return data;
+        const _self = this;
+        let data = await _self.fetchData( dataUrl, start );
+        return data;
 
-	},
-	receiveData( dataUrl, start ) {
-		const _self = this;
-		let data = {};
+    },
+    receiveData( dataUrl, start ) {
+        const _self = this;
+        let data = {};
 
-		_self.getData('virtual/result', 2).then(
-			function(responseThenData) {
-				data = responseThenData;
-			})
-			.then(function() {
-				//console.log('abc')
-			})
-			.catch(function(e) {
-				console.log("promise, error =", e);
-			});
+        _self.getData('virtual/result', 2).then(
+            function(responseThenData) {
+                data = responseThenData;
+            })
+            .then(function() {
+                //console.log('abc')
+            })
+            .catch(function(e) {
+                console.log("promise, error =", e);
+            });
 
-		return data;
-	},
+        return data;
+    },
 
     async sendToServer ( url, method, headers, postBody ) {
         const _self = this;
@@ -52,15 +52,22 @@ module.exports = {
         } catch (e) {
             console.log("Oops, error", e)
         }
-	},
+    },
 
     postFromServer ( requestParam ) {
         const _self = this;
-		let url = requestParam['url'] || '';
+        let url = requestParam['url'] || '';
         let headers = requestParam['headers'] || '';
         let postBody = requestParam['postBody'] || '';
-		let response = _self.sendToServer( url, 'POST', headers, postBody );
-		console.log('第三方response === ', response)
+        let response = _self.sendToServer( url, 'POST', headers, postBody );
+
+        return response.then(function(value) {
+            //console.log('第三方 value === ', value)
+            if( value['code'] === 200 ) {
+                return value
+            }
+        }, function(error) {
+            console.log('第三方 error === ', error)
+        });
     }
 }
-
